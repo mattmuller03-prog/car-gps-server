2ae01deed0a0b8aba245f20b35e92376c2886dcb
Kayla S Rooney <ksrooney@millersville.edu>
2026-05-15 18:37:47 -0400

    Code used to run the online GPS server

---
+++ server.py
@@ -0,0 +1,84 @@
+from flask import Flask, request, jsonify, render_template_string
+
+app = Flask(__name__)
+
+# store latest locations from all cars
+locations = {}
+
+# -----------------------------
+# RECEIVE GPS UPDATES
+# -----------------------------
+@app.route("/update", methods=["POST"])
+def update():
+    data = request.json
+    car_id = data.get("car_id")
+    locations[car_id] = data
+    return {"status": "ok"}
+
+# -----------------------------
+# PROVIDE ALL CAR LOCATIONS
+# -----------------------------
+@app.route("/locations")
+def get_locations():
+    return jsonify(locations)
+
+# -----------------------------
+# LIVE MAP PAGE
+# -----------------------------
+@app.route("/map")
+def map_page():
+    html = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Live GPS Map</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet"
+              href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+    </head>
+    <body>
+        <h2>Live GPS Map</h2>
+        <div id="map" style="height: 90vh;"></div>
+
+        <script>
+            var map = L.map('map').setView([40, -76], 12);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19
+            }).addTo(map);
+
+            var markers = {};
+
+            function updateMarkers() {
+                fetch('/locations')
+                    .then(r => r.json())
+                    .then(data => {
+                        for (const car in data) {
+                            const info = data[car];
+                            const lat = info.lat;
+                            const lon = info.lon;
+
+                            if (!markers[car]) {
+                                markers[car] = L.marker([lat, lon]).addTo(map);
+                            } else {
+                                markers[car].setLatLng([lat, lon]);
+                            }
+
+                            markers[car].bindPopup(
+                                car + "<br>" +
+                                "Speed: " + info.speed + "<br>" +
+                                "Alt: " + info.alt
+                            );
+                        }
+                    });
+            }
+
+            setInterval(updateMarkers, 1000);
+        </script>
+    </body>
+    </html>
+    """
+    return render_template_string(html)
+
+app.run(host="0.0.0.0", port=5000)
